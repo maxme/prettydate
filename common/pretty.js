c4e1a72f630d681c4b29c324c78964237f881123
Maxime Biais <maxime.biais@gmail.com>
2013-12-03 18:52:47 +0100

    remove jQuery ref

--- common/pretty.js
+++ common/pretty.js
@@ -6,7 +6,7 @@
 
 // Takes an ISO time and returns a string representing how
 // long ago the date represents.
-function prettyDate(time){
+function prettyDate(time) {
 	var date = new Date((time || "").replace(/-/g,"/").replace(/[TZ]/g," ")),
 		diff = (((new Date()).getTime() - date.getTime()) / 1000),
 		day_diff = Math.floor(diff / 86400);
@@ -24,13 +24,3 @@ function prettyDate(time){
 		day_diff < 7 && day_diff + " days ago" ||
 		day_diff < 31 && Math.ceil( day_diff / 7 ) + " weeks ago";
 }
-
-// If jQuery is included in the page, adds a jQuery plugin to handle it as well
-if ( typeof jQuery != "undefined" )
-	jQuery.fn.prettyDate = function(){
-		return this.each(function(){
-			var date = prettyDate(this.title);
-			if ( date )
-				jQuery(this).text( date );
-		});
-	};
